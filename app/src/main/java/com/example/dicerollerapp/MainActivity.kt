a82d251ead90ec2b3c07eb31ff870cd3adfad72f
Paul kafanda <paulkaf84@gmail.com>
2023-07-09 21:03:17 +0200

    fix unnecessary variable

--- app/src/main/java/com/example/dicerollerapp/MainActivity.kt
+++ app/src/main/java/com/example/dicerollerapp/MainActivity.kt
@@ -10,16 +10,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -33,13 +27,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.dicerollerapp.ui.theme.DiceRollerAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -67,10 +59,9 @@ fun DiceRollerAndImage(
     modifier: Modifier
 ) {
 
-    var firstDiceNbOfPips by remember { mutableStateOf(1) }
-    var secondDiceNbOfPips by remember { mutableStateOf(1) }
-    var playerTurn by remember { mutableStateOf(1) }
-    val isSecond by remember { mutableStateOf(false) }
+    var firstDiceNbOfPips by remember { mutableStateOf((1..6).random()) }
+    var secondDiceNbOfPips by remember { mutableStateOf((1..6).random()) }
+
     var enabled1 by remember { mutableStateOf(true) }
     var enabled2 by remember { mutableStateOf(false) }
 
